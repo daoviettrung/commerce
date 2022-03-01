f0308d856928f1522f0fdcb0fe43553349db2ebc
System Administrator <root@Daos-MacBook-Air.local>
2022-03-01 21:39:15 +0700

    v4

--- resources/views/layouts/inc/admin/sidebar.blade.php
+++ resources/views/layouts/inc/admin/sidebar.blade.php
@@ -1,19 +1,6 @@
-<div class="menu bg-dark">
-    <div class="dropdown">
-      <button class="dropbtn btn-menu">Dashboard</button>
-    </div>
-    <div class="dropdown">
-      <button class="dropbtn btn-menu">Category</button>
-      <div class="dropdown-content">
-        <a href="{{ url('categories/create') }}">Add category</a>
-        <a href="{{ url('categories') }}">View category</a>
-      </div>
-    </div>
-    <div class="dropdown">
-      <button class="dropbtn btn-menu">Product</button>
-      <div class="dropdown-content">
-        <a href="{{ url('product/create')}}">Add product</a>
-        <a href="{{ url('product')}}">View product</a>
-      </div>
-    </div>
+<div class="menu">
+      <p class="menu-content">Dashboard</p>
+      <p class="menu-content">Dashboard</p>
+      <p class="menu-content">Dashboard</p>
+
   </div>
